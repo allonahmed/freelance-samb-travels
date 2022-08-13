18aec04c28e233fab28dffe8b40491b781c43ff8
allonahmed <allonahmed@mec.science>
2022-08-13 16:46:44 -0400

    pubkey updated

--- src/components/booking/StripeContainer.js
+++ src/components/booking/StripeContainer.js
@@ -3,7 +3,8 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Form from "./Form";
 
-const PUBLIC_KEY = "pk_test_51Kr389Km1rEXqKUQXHOh6eUTxC8A7bx1NWFymt47oTtYCmquJyuQD24kc1MlqHy6JyaP5rtklc2sHBNqdvFRIk2q005Bu07Cq6"
+const PUBLIC_KEY = "pk_live_51Kr389Km1rEXqKUQcTrDImBPRLE7hz9owNj1FcImaHmcjyHVAqUUNi1GcmedSPZPmKJf0LV72NHtIoNQima2q6zJ00x0HsKFEc"
+// "pk_test_51Kr389Km1rEXqKUQXHOh6eUTxC8A7bx1NWFymt47oTtYCmquJyuQD24kc1MlqHy6JyaP5rtklc2sHBNqdvFRIk2q005Bu07Cq6"
 const StripeContainer = ({ active }) => {
   const stripeTestPromise = loadStripe(PUBLIC_KEY);
   return (
